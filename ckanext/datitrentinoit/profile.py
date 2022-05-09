4ce69e5f09c07fc01dc0d2e07e446308bfc566b5
akshayb2022 <akshay.babar@geosolutionsgroup.com>
2022-05-09 18:59:26 +0530

    Fix PR comments.

--- ckanext/datitrentinoit/profile.py
+++ ckanext/datitrentinoit/profile.py
@@ -2,12 +2,9 @@
 import logging
 
 from rdflib.namespace import Namespace, RDF, SKOS
-from rdflib import URIRef, BNode, Literal
+from rdflib import BNode, Literal
 
-import ckan.logic as logic
-
-from ckanext.dcat.profiles import RDFProfile, DCAT, LOCN, VCARD, DCT, FOAF, ADMS, OWL, SCHEMA, TIME
-from ckanext.dcatapit.dcat.profiles import remove_unused_object
+from ckanext.dcat.profiles import RDFProfile, DCAT, VCARD
 
 DCATAPIT = Namespace('http://dati.gov.it/onto/dcatapit#')
 
@@ -24,16 +21,9 @@ class DatitrentinoitProfile(RDFProfile):
 
     def graph_from_dataset(self, dataset_dict, dataset_ref):
         g = self.g
-        org_dict = _get_org(dataset_dict.get('owner_org'))
-
-        # Remove previous poc
-        for s, p, o in g.triples((dataset_ref, DCAT.contactPoint, None)):
-            log.info(f"UmbriaProfile: Removing contactPoint {o}")
-            g.remove((s, p, o))
-            remove_unused_object(g, o, "contactPoint (UMBRIA)")
-
+        
         # Add new poc
-        create_point = dataset_dict.get('contact_point')
+        contact_point = dataset_dict.get('contact_point')
 
         poc = BNode()
         g.add((dataset_ref, DCAT.contactPoint, poc))
@@ -41,24 +31,4 @@ class DatitrentinoitProfile(RDFProfile):
         g.add((poc, RDF.type, VCARD.Kind))
         g.add((poc, RDF.type, VCARD.Organization))
 
-        g.add((poc, VCARD.fn, Literal(create_point if create_point else org_dict.get('create_point'))))
-        
-
-def _get_org(org_id):
-    org_dict = {}
-    if org_id:
-        try:
-            org_dict = logic.get_action('organization_show')(
-                {'ignore_auth': True},
-                {'id': org_id,
-                 'include_datasets': False,
-                 'include_tags': False,
-                 'include_users': False,
-                 'include_groups': False,
-                 'include_extras': True,
-                 'include_followers': False}
-                )
-        except Exception as err:
-            log.warning("Cannot get org for %s: %s", org_id, err, exc_info=err)
-
-    return org_dict
+        g.add((poc, VCARD.fn, Literal(contact_point)))
