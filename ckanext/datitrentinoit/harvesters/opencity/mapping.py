e1e5c5b87b3f754df949b6adf7dc80ae622c6be2
akshayb2022 <akshay.babar@geosolutionsgroup.com>
2022-05-17 17:54:22 +0530

    Resolve PR comments.

--- ckanext/datitrentinoit/harvesters/opencity/mapping.py
+++ ckanext/datitrentinoit/harvesters/opencity/mapping.py
@@ -1,22 +1,30 @@
 import json
+from datetime import datetime
 
 
 def create_opencity_package_dict(harvest_object, config):
     json_dict = json.loads(harvest_object.content)
 
+
+    def dateformat(d):
+        return datetime.fromisoformat(d).strftime('%d-%m-%Y')
+
+    start_date = json_dict["start_date"]
+    end_date = json_dict["end_date"]
+
     package_dict = {
         "title": json_dict["title"],
-        "short_description": json_dict["short_description"],
-        "identifier": json_dict["identifier"],
+        "notes": json_dict['short_description'],
         "geographical_geonames_url": json_dict["spatial_coverage"],
         "url": json_dict["uri"],
     }
 
     extras = {
+        "identifier": json_dict['identifier'],
         "temporal_coverage": [
             {
-                "temporal_start": json_dict["start_date"],
-                "temporal_end": json_dict["end_date"],
+                "temporal_start": dateformat(start_date),
+                "temporal_end": dateformat(end_date),
             }
         ]
     }
